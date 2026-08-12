c1b1204f05a37be70b03e16c35366d69fcdb0533
Andrew Muratov <andrewamuratov@gmail.com>
2026-08-11 21:01:41 -0400

    fix: format production hardening changes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Outlet, Link, createRootRouteWithContext, useRouter, HeadContent, Scripts } from "@tanstack/react-router";
+import {
+  Outlet,
+  Link,
+  createRootRouteWithContext,
+  useRouter,
+  HeadContent,
+  Scripts,
+} from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
 
 import { reportLovableError } from "../lib/lovable-error-reporting";
