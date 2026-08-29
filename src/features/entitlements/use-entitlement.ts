6a4d5b034fe01448c42d260df4f05af135459690
Andrew Muratov <andrewamuratov@gmail.com>
2026-08-29 06:37:02 +0200

    cleanup: remove retired entitlement hook

--- src/features/entitlements/use-entitlement.ts
+++
@@ -1,6 +0,0 @@
-import { FREE_ENTITLEMENT, type Entitlement } from "./entitlements";
-
-/** Current Gapwise features are fully free; historical entitlement rows are not consulted. */
-export function useEntitlement(_userId: string | null, _demo = false): Entitlement {
-  return FREE_ENTITLEMENT;
-}
