85ca366a850aa5f274964f46b53d35532f7997c8
Andrew Muratov <andrewamuratov@gmail.com>
2026-09-01 18:52:44 +0200

    Disambiguate desktop account action from export settings

--- src/components/DesktopSidebar.tsx
+++ src/components/DesktopSidebar.tsx
@@ -124,7 +124,7 @@ export function DesktopSidebar({
             onClick={onOpenAccount}
           >
             <Settings aria-hidden="true" />
-            <span>Account settings</span>
+            <span>Account &amp; sync</span>
           </button>
           <button
             type="button"
