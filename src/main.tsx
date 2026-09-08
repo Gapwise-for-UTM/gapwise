f9cf0032f714edd04642ab30cdf7eb571f5fd305
Andrew Muratov <andrewamuratov@gmail.com>
2026-09-07 21:45:53 -0400

    style: load Gapwise blue brand layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import "./accessibility.css";
 import "./gap-plan-overlays.css";
 import "./clean-ui.css";
 import "./clean-ui-v2.css";
+import "./brand-blue.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
