1f8abff41b8cc428cfc10556cbd07a9f0326689f
Andrew Muratov <andrewamuratov@gmail.com>
2026-09-18 20:59:30 -0400

    docs: rebrand Gapwise for UofT

--- src/routes/_app.index.tsx
+++ src/routes/_app.index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-const TITLE = "Gapwise for UTM";
+const TITLE = "Gapwise for UofT";
 const DESCRIPTION = "Plan UTM timetables, useful gaps, and campus routes in one precise workspace.";
 
 export const Route = createFileRoute("/_app/")({
